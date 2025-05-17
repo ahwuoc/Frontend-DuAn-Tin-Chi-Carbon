3363f5fe41c98e2497b4f135fe1d25cddbfc7657
anhquoc1k98 <anhquoc1k98>
2025-05-17 12:17:20 +0700

    fix

--- app/quan-ly/admin/users/page.tsx
+++ app/quan-ly/admin/users/page.tsx
@@ -47,7 +47,7 @@ export default function AdminUsersPage() {
       try {
         const res = await apiAuth.getAll();
         if (res && res.data) {
-          setUsers(res.data);
+          setUsers(res.data.data);
         } else {
           throw new Error("Không lấy được danh sách người dùng");
         }
